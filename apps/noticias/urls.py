72792401e75a5991d865b13535b2ae92adccb1d9
Federico <fededurnbeck1234@gmail.com>
2023-07-19 12:52:00 -0300

    commit de prueba

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     path('listarNoticia/', views.mostrarNoticia.as_view()), #clase
     path('detalle/<int:pk>', views.DetalleNoticias, name='detalle'),
     path('addNoticia', views.AddNoticia, name='addnoticia'),
+    path('noticias/<int:pk>/edit/', views.EditNoticia, name='edit_noticia'),
     #url comentarios
     path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('comment/add/<int:noticia_id>/', views.add_comment, name='add_comment'),
+    path('comment/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -4,10 +4,13 @@ from django.contrib.auth.decorators import login_required
 from .models import Noticia, Categoria, Comment, Usuario
 from django.views.generic.list import ListView #para las vistas clases
 from .forms import NoticiaForm, CommentForm
+from django.http import HttpResponseForbidden
+from django.contrib import messages
+
 # Create your views here.
 
 #VISTA CON FUNCIONES
-def ListarNoticias(request):
+'''def ListarNoticias(request):
     contexto = {} #diccionario
     id_categoria = request.GET.get("id", None)
 
@@ -21,7 +24,42 @@ def ListarNoticias(request):
     cat = Categoria.objects.all().order_by('nombre') #ordena por nombre
     contexto['categorias'] = cat
 
-    return render(request, 'noticias/listar.html', contexto)
+    return render(request, 'noticias/listar.html', contexto)'''
+
+def ListarNoticias(request):
+    noticias = Noticia.objects.all()
+
+    # Filtrar por categoría
+    categoria = request.GET.get('categoria')
+    if categoria:
+        noticias = noticias.filter(categoria_noticia=categoria)
+
+    # Filtrar por antigüedad ascendente
+    antiguedad_asc = request.GET.get('antiguedad_asc')
+    if antiguedad_asc:
+        noticias = noticias.order_by('fecha_de_publicacion')
+
+    # Filtrar por antigüedad descendente
+    antiguedad_desc = request.GET.get('antiguedad_desc')
+    if antiguedad_desc:
+        noticias = noticias.order_by('-fecha_de_publicacion')
+
+    # Filtrar por orden alfabético ascendente
+    orden_asc = request.GET.get('orden_asc')
+    if orden_asc:
+        noticias = noticias.order_by('titulo')
+
+    # Filtrar por orden alfabético descendente
+    orden_desc = request.GET.get('orden_desc')
+    if orden_desc:
+        noticias = noticias.order_by('-titulo')
+
+    context = {
+        'noticias': noticias,
+        'categorias': Categoria.objects.all(),  # Asegúrate de tener el modelo 'Categoria' importado y definido
+    }
+    return render(request, 'noticias/listar.html', context)
+
 
 #VISTA CON CLASES
 class mostrarNoticia(ListView):
@@ -85,6 +123,30 @@ def add_comment(request, noticia_id):
     return redirect('noticias:detalle', pk=noticia_id)
 
 
+# EDITAR COMENTARIOS
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    #mensaje de error si no sos el autor
+    if comment.author != request.user.username:
+        messages.error(request, 'No tienes permisos para editar este comentario.')
+        return redirect('noticias:detalle', pk=comment.noticia.pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('noticias:detalle', pk=comment.noticia.pk)
+    else:
+        form = CommentForm(instance=comment)
+
+    context = {
+        'form': form,
+        'comment': comment,
+    }
+    return render(request, 'noticias/edit_comment.html', context)
+
 @login_required
 def AddNoticia(request):
     if request.method == 'POST':
@@ -98,3 +160,24 @@ def AddNoticia(request):
         form = NoticiaForm()
     
     return render(request, 'noticias/addNoticia.html', {'form': form})
+
+@login_required
+def EditNoticia(request, pk):
+    noticia = get_object_or_404(Noticia, pk=pk)
+
+    # Solo el autor puede editar la noticia
+    if noticia.author != request.user:
+        return HttpResponseForbidden("No tenes permiso para editar esta noticia.")
+
+    if request.method == 'POST':
+        form = NoticiaForm(request.POST, request.FILES, instance=noticia)
+        if form.is_valid():
+            form.save()
+            return redirect('noticias:detalle', pk=pk)
+    else:
+        form = NoticiaForm(instance=noticia)
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'noticias/editNoticia.html', context)
